40c4e47211e3939970c5acc7ac016b4b662c62d3
Yigal Chripun <579788+yigal100@users.noreply.github.com>
2025-04-16 12:47:19 +1000

    feat: add sample MathExp grammar with lexer and parser, add binary files

--- compiler/src/lib.rs
+++ compiler/src/lib.rs
@@ -1,3 +1,35 @@
+#![forbid(unsafe_code)]
+#![warn(clippy::pedantic)]
+
+#[allow(dead_code)]
+mod math_exp;
+
+use hime_redist::ast::AstNode;
+
 pub fn greet_user(name: &str) {
     println!("Hello, {}!", name);
-}   
+    let result = math_exp::parse_str("2 + 3");
+    let ast = result.get_ast();
+    let root = ast.get_root();
+    print(root, &[]);
+}
+
+fn print(node: AstNode, crossings: &[bool]) {
+    let mut i = 0;
+    if !crossings.is_empty() {
+        while i < crossings.len() - 1 {
+            print!("{:}", if crossings[i] { "|   " } else { "    " });
+            i += 1;
+        }
+        print!("+-> ");
+    }
+    println!("{node}");
+    i = 0;
+    let children = node.children();
+    while i < children.len() {
+        let mut child_crossings = crossings.to_owned();
+        child_crossings.push(i < children.len() - 1);
+        print(children.at(i), &child_crossings);
+        i += 1;
+    }
+}
